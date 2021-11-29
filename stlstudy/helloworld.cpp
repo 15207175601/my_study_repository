c7a18c55eb7c73f77ebe05ca18e5d1c172cfc9e7
Zhun Liu <852780186@qq.com>
2021-11-28 18:04:23 -0800

    traits_template

--- stlstudy/helloworld.cpp
+++ stlstudy/helloworld.cpp
@@ -1,7 +1,29 @@
 #include<iostream>
-
+#include"myadapter.hpp"
 int main(){
-	std::cout<<"helloworld"<<std::endl;
+	using namespace MySTL;
+	using std::cout;
+	using std::endl;
+	plus<int> plusobj;
+	minus<int> minusobj;
+	multiplies<int> multipliesobj;
+	divides<int> dividesobj;
+	modulus<int> modulusobj;
+	negate<int> negateobj;
+
+	cout<<plusobj(3,5)<<endl;
+	cout<<minusobj(3,5)<<endl;
+	cout<<multipliesobj(3,5)<<endl;
+	cout<<dividesobj(3,5)<<endl;
+	cout<<modulusobj(3,5)<<endl;
+	cout<<negateobj(3)<<endl;
+
+	cout<<equal_to<int>()(3,5)<<endl;
+	cout<<not_equal_to<int>()(3,5)<<endl;
+	cout<<greater<int>()(3,5)<<endl;
+	cout<<less<int>()(3,5)<<endl;
+	cout<<greater_equal<int>()(3,5)<<endl;
+	cout<<less_equal<int>()(3,5)<<endl;
 	return 0;
 }
 
--- stlstudy/traits_test.cpp
+++ stlstudy/traits_test.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-template <typename T>
+/*template <typename T>
 void func(T t){
     std::cout<<t<<std::endl;
 }
@@ -21,4 +21,39 @@ int main(){
     func(x);
     func(p);
     return 0;
+}*/
+
+template <class I,class T>
+void func_impl(I iter,T t){
+    T temp;
+    //...
+}
+
+template<class I>
+void func(I iter){
+    func_impl(iter,*iter);
+}
+
+template <class I>
+struct iterator_traits{
+    typedef typename I::iterator_category   iterator_category;
+    typedef typename I::value_type          value_type;
+    typedef typename I::difference_type     difference_type;
+    typedef typename I::pointer             pointer;
+    typedef typename I::reference           reference;
+};
+
+template <class T>
+struct iterator_traits<T*>{
+    typedef typename random_access_iterator_tag iterator_category;
+    typedef typename T                          value_type;
+    typedef typename ptrdiff_t                  difference_type;
+    typedef typename T*                         pointer;
+    typedef typename T&                         reference;
+};
+
+template <class I>
+typename iterator_traits<I>::value_type
+func(I iter){
+    return *iter;
 }
