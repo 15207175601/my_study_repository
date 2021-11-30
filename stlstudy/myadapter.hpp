22757b0ca44db8a9b39ec80882c57a2688d0aa2e
Zhun Liu <852780186@qq.com>
2021-11-29 17:27:17 -0800

    myadapter/2

--- stlstudy/myadapter.hpp
+++ stlstudy/myadapter.hpp
@@ -96,30 +96,30 @@ namespace MySTL{
     };
 
     template <class T>
-    struct identity:public unary_function<T,TIME_UTC>{
+    struct identity:public unary_function<T,T>{
         const T& operator()(const T&x) const{return x;}
     };
 
-    template <class T>
+    template <class Pair>
     struct select1st:public unary_function<Pair,typename Pair::first_type>{
         const typename Pair::first_type& operator()(const Pair&x) const{return x.first;}
     };
 
-    template <class T>
+    template <class Pair>
     struct select2nd:public unary_function<Pair,typename Pair::second_type>{
         const typename Pair::second_type& operator()(const Pair&x) const{return x.second;}
     };
 
-    template <class T>
+    template <class Arg1,class Arg2>
     struct project1st:public binary_function<Arg1,Arg2,Arg1>{
         Arg1& operator()(const Arg1&x,const Arg2&y) const{return x;}
     };
 
-    template <class T>
+    template <class Arg1,class Arg2>
     struct project2nd:public binary_function<Arg1,Arg2,Arg1>{
         Arg2& operator()(const Arg1&x,const Arg2&y) const{return y;}
     };
 
-    
+
 }
 #endif //MYADAPTER
